8571959f5c5ff2d66b2a9217516c482f33532970
serge <kislak7@gmail.com>
2021-09-23 16:42:47 +0300

    fr

--- quickstart.rb
+++ quickstart.rb
@@ -15,12 +15,24 @@ service = Google::Apis::SheetsV4::SheetsService.new
 service.authorization = authorizer
 
 # https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-SOURCE = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-RANGE = "Class Data!A2:E"
+#
+
+"https://docs.google.com/spreadsheets/d/1L60GiPM2Tu3arPkZwCRKkSdfCuIty9QmPUzXp8pbAfk/edit?usp=sharing"
+
+# SOURCE = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+# RANGE = "Class Data!A2:E"
+
+SOURCE = "1L60GiPM2Tu3arPkZwCRKkSdfCuIty9QmPUzXp8pbAfk"
+RANGE = "fr!A1:C100"
+
 response = service.get_spreadsheet_values(SOURCE, RANGE)
 
-response.values.each do |row|
-  puts "#{row[0]}, #{row[4]}"
+response.values.map do |row|
+  next if row.first.empty?
+  puts row.join("\n  ")
 end
+
+# row = response.values.sample
+# puts row.join("\n  ")
 
 puts "No data found." if response.values.empty?
