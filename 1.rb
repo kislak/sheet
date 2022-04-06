219696d2243716aea0633daa88d5934ea1afb6b4
Serge Kislak <kislak7@gmail.com>
2022-04-06 10:16:47 +0300

    cleanup

--- 1.rb
+++ 1.rb
@@ -2,7 +2,9 @@
 # https://www.youtube.com/watch?v=VqoSUSy011I
 
 require 'google_drive'
-session = GoogleDrive::Session.from_service_account_key("credentials.json")
+require 'pry'
+
+session = GoogleDrive::Session.from_service_account_key("#{Dir.home}/sec/google_service.json")
 s = session.spreadsheet_by_title("Freshly vocab")
 ws = s.worksheets.first
 ws.rows[200..207].each do |i|
