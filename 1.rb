12d0c5403cf9787a054a7dc01c31add399d63c1f
Serge Kislak <kislak7@gmail.com>
2022-04-06 15:49:24 +0300

    ok

--- 1.rb
+++ 1.rb
@@ -6,10 +6,24 @@ require 'pry'
 
 session = GoogleDrive::Session.from_service_account_key("sec/google_service.json")
 
-s = session.spreadsheet_by_title("Freshly vocab")
-ws = s.worksheets.first
-ws.rows[200..207].each do |i|
-  sleep 3
-  #   say -v '?'
-  `say -v Alex '#{i.first}'`
-end
+book = session.spreadsheet_by_title("tally")
+state = book.worksheet_by_title('state')
+links = book.worksheet_by_title('links')
+
+state[1,1] = state[1,1].to_i + 1
+state.save
+
+puts links.rows[0].join(' | ')
+
+
+# ws.rows[0..2].each do |i|
+  # i.first = i.first.to_i + 1p
+# end
+
+
+
+# ws.rows[200..207].each do |i|
+#   sleep 3
+#   #   say -v '?'
+#   # `say -v Alex '#{i.first}'`
+# end
